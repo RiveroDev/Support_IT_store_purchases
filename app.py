e489e77a29b0eb77ced22f401eff66f729cf1a50
RichardRivero <Richard_armando@hotmail.com>
2021-11-13 11:37:42 -0400

    folder templates and params index

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
@@ -8,14 +8,25 @@ app = Flask(__name__)
 #creamos vistas 
 #"@decorador <direccion url>"
 #medotod o funcion
+
+# plantilla original 
+@app.route("/")
+def index():
+    
+    data = {
+        "titulo":"index",
+        "bienvenida":"saludos"
+    }
+    
+    #return "<p>Pagina de inicio</p>"
+    return render_template("index.html", data = data) # dirige la conexion a templeta de la carpeta template
+        # le pasamos a la plantilla inde.html la variable data
+
 @app.route("/compras")
 def compras():
     return "<h1><p>Hola esto es una prueba de red  'Hola Caro'</p></h1>"
 
 
-@app.route("/")
-def index():
-    return "<p>Pagina de inicio</p>"
 
 
 @app.route("/<string:nombre>/")
